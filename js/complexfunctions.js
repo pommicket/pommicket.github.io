83b39b3c7ae56fba695eebd4288797fcb2ff6a69
pommicket <leonardomtenenbaum@gmail.com>
2016-10-16 17:06:31 -0400

    Fixed RPN when aspect ratio =/= 1:1

--- js/complexfunctions.js
+++ js/complexfunctions.js
@@ -11,7 +11,7 @@ function drawPoints(points)
     background(255);
     for (var i = 0; i < points.length; i++)
     {
-        point((points[i][0]/(SCALE*width/750)+0.5)*width, (points[i][1]/(SCALE*height/750)+0.5)*height);
+        point((points[i][0]/(SCALE*width/750)+0.5)*width, (points[i][1]/(SCALE*width/750)+0.5)*height);
     }
 }
 
@@ -23,7 +23,7 @@ function drawManyPoints(points)
         stroke(colors[f][0], colors[f][1], colors[f][2]);
         for (var i = 0; i < points[f].length; i++)
         {
-            point((points[f][i][0]/SCALE+0.5)*width, (points[f][i][1]/SCALE+0.5)*height);
+            point((points[f][i][0]/(SCALE*width/750)+0.5)*width, (points[f][i][1]/(SCALE*width/750)+0.5)*height);
         }
     }
 }
@@ -87,8 +87,14 @@ function makeCanvas(w, h)
     {
         for (var j = 0; j < height; j++)
         {
-            domainPoints.push([(j/width-0.5)*SCALE*width/750, (i/height-0.5)*SCALE*height/750]);
-            domainPoints.push([(i/width-0.5)*SCALE*width/750, (j/height-0.5)*SCALE*height/750]);
+            domainPoints.push([(i/width-0.5)*SCALE*width/750, (j/height-0.5)*SCALE*width/750]);
+        }
+    }
+    for (var i = 10; i < height; i += 50)
+    {
+        for (var j = 0; j < width; j++)
+        {
+            domainPoints.push([(j/width-0.5)*SCALE*width/750, (i/height-0.5)*SCALE*width/750]);
         }
     }
     drawPoints(domainPoints);
