a499a19220f216193cfb604e63590d48ccebacac
Leo Tenenbaum <leonardomtenenbaum@gmail.com>
2017-09-19 23:23:47 -0400

    Added viewport

--- navbar.js
+++ navbar.js
@@ -1,4 +1,5 @@
 $(function()
 {
     $("#navbar").load("https://pommicket.github.io/Navbar.html");
+    $("head").append('<meta name="viewport" content="width=device-width, initial-scale=1.0">');
 });
